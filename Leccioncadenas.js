5f935273c537c7cd273671879e2bad5c81bbe4e7
alexpino1 <56764448+alexpino1@users.noreply.github.com>
2023-06-28 22:33:50 -0500

    Add files via upload

---
+++ Leccioncadenas.js
@@ -0,0 +1,15 @@
+let Nombre=" Walter_Alexander";
+let Apellido="Pino_Vaquez ";
+let estudiante=Nombre.concat(" ",Apellido);
+console.log(estudiante)
+let estudianteMayus=estudiante.toUpperCase(estudiante);
+console.log(estudianteMayus)
+let estudianteMinus=estudiante.toLowerCase(estudiante);
+console.log(estudianteMinus)
+let contarletras=estudiante.length;
+console.log(contarletras);
+console.log(Nombre.charAt(0));
+console.log(estudiante.trim());
+console.log(estudiante.includes("Walter_Alexander"));
+
+
