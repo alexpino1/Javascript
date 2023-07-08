012f9b39bb65437cade275c13cc23af5bdb42511
alexpino1 <56764448+alexpino1@users.noreply.github.com>
2023-07-08 17:46:16 -0500

    Add files via upload

---
+++ ejercicios/Ejercicio2_OperadoresConversionNumericos.js
@@ -0,0 +1,17 @@
+let alturacm = 168;
+console.log(typeof(alturacm));
+console.log(alturacm);
+let alturaM = parseFloat(alturacm);;
+console.log(typeof(alturaM));
+console.log(alturaM);
+
+//peso  float 
+let peso =70.85;
+console.log (parseFloat(peso));
+//redondiar hacia arriba 
+console.log(Math.round(peso));
+//redondiar hacia abajo
+console.log(Math.floor(peso));
+// maximo valor 
+let max_valor_JS = Number.MAX_VALUE;
+console.log(max_valor_JS);
